17bc2136aeb69fc2b3495fd232bef0d04b30be52
Silas Ferreira <silasrf1@gmail.com>
2024-04-23 10:08:16 -0300

    feat: include icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,6 +2,10 @@ const Footer = () => {
     return(
         <footer>
             <div>
+                <i className="fa-solid fa-angle-up"></i>
+                <i className="fa-solid fa-angle-down"></i>
+            </div>
+            <div>
                 <p>Obrigado por acessar!</p>
             </div>
         </footer>
