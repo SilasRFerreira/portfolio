18a753a735e7bbd7ef16fe2cdff660cb40d37ced
Silas Ferreira <silasrf1@gmail.com>
2024-04-23 09:09:50 -0300

    build: build skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import Greetings from "./HomeSections/Greetings";
 
 export default function Home() {
   return (
     <main>
       <Header/>
+      <Greetings/>
     </main>
   );
 }
