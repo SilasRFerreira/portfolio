3bb96547d3d6235b1f111912b66c4fb39e3d1d9b
Silas Ferreira <silasrf1@gmail.com>
2024-04-23 09:29:30 -0300

    build: created basic structure/ put some tecnology icons

---
+++ src/app/HomeSections/Tecnologies/index.tsx
@@ -0,0 +1,19 @@
+const Tecnologies = () => {
+    return (
+        <section>
+            <div>
+                <h2>Ferramentas que domino</h2>
+                <div>
+                    <i className="fa-brands fa-html5"></i>
+                    <i className="fa-brands fa-css3-alt"></i>
+                    <i className="fa-brands fa-react"></i>
+                    <i className="fa-brands fa-js"></i>
+                    <i className="fa-brands fa-python"></i>
+                    <i className="fa-brands fa-node"></i>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Tecnologies
