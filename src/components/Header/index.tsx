ecc27bf6c5652e34815c0f070af849471f7fee1d
Silas Ferreira <silasrf1@gmail.com>
2024-04-23 08:59:04 -0300

    feat: added links to icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,11 +7,15 @@ const Header = () => {
                     <h3>Silas Ferreira</h3>
                 </div>
                 <div>
-                    <a href="https://www.google.com">
+                    <a href="https://wa.me/5521968383002" target="_blank">
                         <i className="fa-brands fa-whatsapp"></i>
                     </a>
-                    <i className="fa-brands fa-github"></i>
-                    <i className="fa-brands fa-linkedin"></i>
+                    <a href="https://github.com/SilasRFerreira" target="_blank">
+                        <i className="fa-brands fa-github"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/silas-ferreira-1b4552101/" target="_blank">
+                        <i className="fa-brands fa-linkedin"></i>
+                    </a>
                 </div>
             </div>
         </header>
